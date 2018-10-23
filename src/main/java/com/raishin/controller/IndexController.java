3925f48144bb3e9e25985d228d6f6b992696347c
raishin <raishin7773@yahoo.co.jp>
2018-10-23 19:39:47 +0900

    logback設定

--- src/main/java/com/raishin/controller/IndexController.java
+++ src/main/java/com/raishin/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.raishin.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +21,8 @@ public class IndexController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Model model) {
+		Logger logger = LoggerFactory.getLogger("Main");
+		logger.info("loggerTest");
 		List<Fruit> list = fruitMapper.selectAll();
 		model.addAttribute("fruits", list);
 		return "index";
