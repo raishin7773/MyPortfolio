ac2bab841fc54a76d9e8793fe0421d678402c4a5
raishin <raishin7773@yahoo.co.jp>
2018-11-09 22:40:57 +0900

    11/09　labelsにオブジェクトを指定したい

--- src/main/java/com/raishin/controller/DeckController.java
+++ src/main/java/com/raishin/controller/DeckController.java
@@ -1,8 +1,10 @@
 package com.raishin.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +19,15 @@ public class DeckController {
   DeckMapper mapper;
 
   @RequestMapping(value = "/deck/list")
-  public String index(@ModelAttribute("deckForm") DeckForm form, BindingResult result) {
+  public String index(@ModelAttribute("deckForm") DeckForm form, BindingResult result,
+      Model model) {
+    List<String> deckNameList = new ArrayList<>();
+    List<Integer> duelNumberList = new ArrayList<>();
     List<DeckEntity> entityList = mapper.selectAll();
+    entityList.stream().forEach(x -> deckNameList.add(x.getDeckName()));
+    entityList.stream().forEach(x -> duelNumberList.add(x.getWin() + x.getLose() + x.getDraw()));
+    model.addAttribute("deckNameList", deckNameList);
+    model.addAttribute("duelNumberList", duelNumberList);
     form.setDeckList(entityList);
     return "starter";
   }
