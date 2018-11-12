068a7897f67f1bbd809332945c749aed23d7e5c3
raishin <raishin7773@yahoo.co.jp>
2018-11-12 22:17:39 +0900

    src → th:src
    href →　th:href
    chart追加
    サイドバーの遷移追加

--- src/main/java/com/raishin/controller/DeckController.java
+++ src/main/java/com/raishin/controller/DeckController.java
@@ -2,6 +2,7 @@ package com.raishin.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,16 +19,27 @@ public class DeckController {
   @Autowired
   DeckMapper mapper;
 
+  Random random = new Random();
+
   @RequestMapping(value = "/deck/list")
   public String index(@ModelAttribute("deckForm") DeckForm form, BindingResult result,
       Model model) {
     List<String> deckNameList = new ArrayList<>();
+    List<String> backColorList = new ArrayList<>();
     List<Integer> duelNumberList = new ArrayList<>();
+    List<Double> winningPercentageList = new ArrayList<>();
     List<DeckEntity> entityList = mapper.selectAll();
     entityList.stream().forEach(x -> deckNameList.add(x.getDeckName()));
     entityList.stream().forEach(x -> duelNumberList.add(x.getWin() + x.getLose() + x.getDraw()));
+    entityList.stream().forEach(x -> winningPercentageList.add(((double) x.getWin()
+        / ((double) x.getWin() + (double) x.getLose() + (double) x.getDraw()) * 100)));
+    entityList.stream().forEach(x -> backColorList.add("rgb(" + random.nextInt(256) + ", "
+        + random.nextInt(256) + ", " + random.nextInt(256) + ")"));
+
+    model.addAttribute("backColorList", backColorList);
     model.addAttribute("deckNameList", deckNameList);
     model.addAttribute("duelNumberList", duelNumberList);
+    model.addAttribute("winningPercentageList", winningPercentageList);
     form.setDeckList(entityList);
     return "starter";
   }
--- src/main/java/com/raishin/controller/IndexController.java
+++ src/main/java/com/raishin/controller/IndexController.java
@@ -8,7 +8,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.raishin.entity.IndexEntity;
 import com.raishin.form.IndexForm;
 import com.raishin.mapper.IndexMapper;
@@ -16,32 +15,33 @@ import com.raishin.mapper.IndexMapper;
 @Controller
 public class IndexController {
 
-	@Autowired
-	IndexMapper indexMapper;
+  @Autowired
+  IndexMapper indexMapper;
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(@ModelAttribute("indexForm") IndexForm form, BindingResult result) {
-		// Logger logger = LoggerFactory.getLogger("Main");
-		// logger.info("loggerTest");
-		// List<Fruit> list = fruitMapper.selectAll();
-		// model.addAttribute("fruits", list);
-		return "index";
-	}
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String index(@ModelAttribute("indexForm") IndexForm form, BindingResult result) {
+    // Logger logger = LoggerFactory.getLogger("Main");
+    // logger.info("loggerTest");
+    // List<Fruit> list = fruitMapper.selectAll();
+    // model.addAttribute("fruits", list);
+    return "index";
+  }
 
-	@RequestMapping(value = "/login")
-	public String login(@ModelAttribute("indexForm") @Validated IndexForm form, BindingResult result, Model model) {
-		if (result.hasErrors()) {
-			return "index";
-		}
-		IndexEntity entity = indexMapper.selectLogin(form);
-		if (entity == null || !entity.getEmail().equals(form.getEmail())
-				|| !entity.getPassword().equals(form.getPassword())) {
-			result.rejectValue("email", "notlogin");
-			return "index";
-		}
+  @RequestMapping(value = "/login")
+  public String login(@ModelAttribute("indexForm") @Validated IndexForm form, BindingResult result,
+      Model model) {
+    if (result.hasErrors()) {
+      return "index";
+    }
+    IndexEntity entity = indexMapper.selectLogin(form);
+    if (entity == null || !entity.getEmail().equals(form.getEmail())
+        || !entity.getPassword().equals(form.getPassword())) {
+      result.rejectValue("email", "notlogin");
+      return "index";
+    }
 
-		// return "redirect:/deck/list";
-		return "forward:/deck/list";
-	}
+    // return "redirect:/deck/list";
+    return "redirect:/deck/list";
+  }
 
 }
