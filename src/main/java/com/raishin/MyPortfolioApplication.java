18bb1bea8b23f5f551b51b5e07c066f8332fa5bf
raishin <raishin7773@yahoo.co.jp>
2018-11-27 21:12:03 +0900

    起動クラス名変更

--- src/main/java/com/raishin/MyPortfolioApplication.java
+++ src/main/java/com/raishin/MyPortfolioApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MybatistestApplication {
+public class MyPortfolioApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MybatistestApplication.class, args);
+    SpringApplication.run(MyPortfolioApplication.class, args);
   }
 }
