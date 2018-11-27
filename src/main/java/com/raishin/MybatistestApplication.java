e55daf1d52f670e410bec4586a59b852644ef755
raishin <raishin7773@yahoo.co.jp>
2018-11-27 21:02:27 +0900

    微修正

--- src/main/java/com/raishin/MybatistestApplication.java
+++ src/main/java/com/raishin/MybatistestApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MybatistestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MybatistestApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MybatistestApplication.class, args);
+  }
 }
--- src/main/java/com/raishin/controller/IndexController.java
+++ src/main/java/com/raishin/controller/IndexController.java
@@ -2,13 +2,10 @@ package com.raishin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.raishin.entity.IndexEntity;
 import com.raishin.form.IndexForm;
 import com.raishin.mapper.IndexMapper;
 
@@ -20,28 +17,25 @@ public class IndexController {
 
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String index(@ModelAttribute("indexForm") IndexForm form, BindingResult result) {
-    // Logger logger = LoggerFactory.getLogger("Main");
-    // logger.info("loggerTest");
-    // List<Fruit> list = fruitMapper.selectAll();
-    // model.addAttribute("fruits", list);
-    return "index";
-  }
-
-  @RequestMapping(value = "/login")
-  public String login(@ModelAttribute("indexForm") @Validated IndexForm form, BindingResult result,
-      Model model) {
-    if (result.hasErrors()) {
-      return "index";
-    }
-    IndexEntity entity = indexMapper.selectLogin(form);
-    if (entity == null || !entity.getEmail().equals(form.getEmail())
-        || !entity.getPassword().equals(form.getPassword())) {
-      result.rejectValue("email", "notlogin");
-      return "index";
-    }
-
-    // return "redirect:/deck/list";
     return "redirect:/useTools/index";
   }
+
+  // @RequestMapping(value = "/login")
+  // public String login(@ModelAttribute("indexForm") @Validated IndexForm form, BindingResult
+  // result,
+  // Model model) {
+  // if (result.hasErrors()) {
+  // return "index";
+  // }
+  // IndexEntity entity = indexMapper.selectLogin(form);
+  // if (entity == null || !entity.getEmail().equals(form.getEmail())
+  // || !entity.getPassword().equals(form.getPassword())) {
+  // result.rejectValue("email", "notlogin");
+  // return "index";
+  // }
+  //
+  // // return "redirect:/deck/list";
+  // return "redirect:/useTools/index";
+  // }
 
 }
