262c0bda15e6beabad9588dd5521a882ddeaa5f0
raishin <raishin7773@yahoo.co.jp>
2018-12-09 18:13:47 +0900

    テンプレート名変更。メール送信処理復活

--- src/main/java/com/raishin/controller/InquiryController.java
+++ src/main/java/com/raishin/controller/InquiryController.java
@@ -1,5 +1,8 @@
 package com.raishin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 // import org.springframework.mail.MailSender;
 // import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Controller;
@@ -16,35 +19,35 @@ public class InquiryController {
   @RequestMapping(value = "/inquiry/index")
   public String index(@ModelAttribute("inquiryForm") InquiryForm form, BindingResult result,
       Model model) {
-    return "inquiry_index";
+    return "inquiry/index";
   }
 
   @RequestMapping(value = "/inquiry/send")
   public String send(@ModelAttribute("inquiryForm") InquiryForm form, BindingResult result,
       Model model) {
-    return "inquiry_confirm";
+    return "inquiry/confirm";
   }
 
   @RequestMapping(value = "/inquiry/confirm", params = "cansel", method = RequestMethod.POST)
   public String cansel(@ModelAttribute("inquiryForm") InquiryForm form, BindingResult result,
       Model model) {
-    return "inquiry_index";
+    return "inquiry/index";
   }
 
-  // @Autowired
-  // private MailSender sender;
+  @Autowired
+  private MailSender sender;
 
   @RequestMapping(value = "/inquiry/confirm", params = "send", method = RequestMethod.POST)
   public String sendmail(@ModelAttribute("inquiryForm") InquiryForm form, BindingResult result,
       Model model) {
-    // SimpleMailMessage msg = new SimpleMailMessage();
-    //
-    // // msg.setFrom(form.getEmail());
-    // msg.setTo("raishin7773@gmail.com");
-    // msg.setSubject("問い合わせ"); // タイトルの設定
-    // msg.setText("email:" + form.getEmail() + " 問い合わせ内容" + form.getInquiry()); // 本文の設定
-    //
-    // this.sender.send(msg);
-    return "inquiry_result";
+    SimpleMailMessage msg = new SimpleMailMessage();
+
+    // msg.setFrom(form.getEmail());
+    msg.setTo("raishin7773@gmail.com");
+    msg.setSubject("問い合わせ"); // タイトルの設定
+    msg.setText("email:" + form.getEmail() + " 問い合わせ内容" + form.getInquiry()); // 本文の設定
+
+    this.sender.send(msg);
+    return "inquiry/result";
   }
 }
